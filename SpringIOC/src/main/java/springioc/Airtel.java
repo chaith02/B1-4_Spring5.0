19cf0ca9f614c1d60a2723ebad95402ce85edfb7
User <User@LAPTOP-6K1H1QK0>
2022-06-09 10:51:59 +0530

    first commit

---
+++ SpringIOC/src/main/java/springioc/Airtel.java
@@ -0,0 +1,24 @@
+package springioc;
+
+public class Airtel implements Sim
+{
+	public Airtel() {
+		super();
+		System.out.println("Airtel class Constructor");
+	}
+
+
+	public void calling() {
+		// TODO Auto-generated method stub
+		System.out.println("Calling using Airtel Sim");
+		
+	}
+
+	public void data() {
+		// TODO Auto-generated method stub
+		System.out.println("data using Airtel Sim");
+		
+	}
+	
+
+}
---
+++ SpringIOC/src/main/java/springioc/Jio.java
@@ -0,0 +1,19 @@
+package springioc;
+
+public class Jio implements Sim 
+{
+
+	public void calling() {
+		// TODO Auto-generated method stub
+		System.out.println("Calling using Jio Sim");
+		
+	}
+
+	public void data() {
+		// TODO Auto-generated method stub
+		System.out.println("data using Jio Sim");
+		
+	}
+
+	
+}
