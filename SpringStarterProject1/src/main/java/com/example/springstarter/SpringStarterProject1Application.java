a5beebcc8cd8f1e1314284a5669188d7e0527cb4
User <User@LAPTOP-6K1H1QK0>
2022-06-28 08:54:03 +0530

    first commit

---
+++ SpringStarterProject1/src/main/java/com/example/springstarter/SpringStarterProject1Application.java
@@ -0,0 +1,15 @@
+package com.example.springstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStarterProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStarterProject1Application.class, args);
+		int a=55;
+		System.out.println(a);
+	}
+
+}
